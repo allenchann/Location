479682e88da1c506cd16e112370c68300de4ced0
allen_Chan <mac@allen.local>
2016-08-23 16:54:26 +0800

    geoCoder
    geoCoder

--- Location/Location/AppDelegate.m
+++ Location/Location/AppDelegate.m
@@ -7,8 +7,13 @@
 //
 
 #import "AppDelegate.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface AppDelegate ()
+@interface AppDelegate ()<CLLocationManagerDelegate>
+
+{
+    CLLocationManager *_locationManager;
+}
 
 @end
 
@@ -17,8 +22,76 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+
+    _locationManager = [[CLLocationManager alloc] init];
+    
+    //设置代理
+    
+    _locationManager.delegate = self;
+    
+    //询问用户，获得权限。会有一个弹窗，询问用户是否允许app获取当前地理位置
+    
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 8) {
+        //由于IOS8中定位的授权机制改变 需要进行手动授权
+        [_locationManager requestAlwaysAuthorization];
+        [_locationManager requestWhenInUseAuthorization];
+    }
+    else
+        [_locationManager requestWhenInUseAuthorization];
+    
+    [_locationManager startUpdatingLocation];
+    
+    
+
+    
+    CLGeocoder *coder = [[CLGeocoder alloc]init];
+    NSString *str = @"中国广东省广州市天河区天园街道中山大道西";
+    //编码器开始编码,在返回的块中进行操作.
+    [coder geocodeAddressString:str completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        //判断是否编码出错
+        if (error) {
+            NSLog(@"%@",error);
+        }
+        else
+        {
+            //从解析的数据中获取到位置信息
+            CLPlacemark *myPlace = [placemarks lastObject];
+            CLLocation *location = myPlace.location;
+            //编码器进行反地理编码,在返回的块中进行操作.
+            [coder reverseGeocodeLocation:location completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+                if (!error)
+                {
+                    //从解析的数据中获取到位置信息
+                    CLPlacemark *place = [placemarks lastObject];
+                    NSLog(@"%@",place.addressDictionary);
+                }
+                else
+                    NSLog(@"%@",error);
+                
+            }];
+        }
+    }];
+    
+    
     return YES;
 }
+
+//定位失败
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    NSLog(@"%@",error);
+}
+//定位完成
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations
+{
+    CLLocation *location = [locations lastObject];
+//    NSLog(@"%@",locations);
+    NSLog(@"%f,%f",location.coordinate.longitude,location.coordinate.latitude);
+    
+    
+    [manager stopUpdatingLocation];
+}
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
